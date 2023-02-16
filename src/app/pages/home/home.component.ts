89f5f20bcc3defd927b2d7650e6431afd5044052
Bhupendra <bhupendra@gamil.com>
2023-02-16 18:58:45 +0530

    integrated the post API with backend server

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QuestionService } from 'src/app/services/question.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+constructor(public questionservice :QuestionService ,public userservice: UserService){
 
+}
   question:string= "";
+  post(){
+    this.questionservice.postQuestion({
+      username :this.userservice.user.username,
+      question :this.question,
+      solutions: []
+    }).subscribe((res)=>{
+      console.log(res);
+      console.log("post question");
+      this.question ="";
+    })
+  }
 }
